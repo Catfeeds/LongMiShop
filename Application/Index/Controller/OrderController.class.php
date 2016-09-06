a3c35ff635246b3bb07b69dfc4aeee4d71ab12ba
钟瀚涛 <651799656@qq.com>
2016-09-06 19:08:57 +0800

    [修改]订单页面修改

--- Application/Index/Controller/OrderController.class.php
+++ Application/Index/Controller/OrderController.class.php
@@ -99,4 +99,21 @@ class OrderController extends BaseIndexController {
     }
 
 
+
+    public function returnGoodsList(){
+        $count = M('return_goods')->where("user_id = {$this->user_id}")->count();
+        $page = new Page($count,10);
+        $list = M('return_goods')->where("user_id = {$this->user_id}")->order("id desc")->limit("{$page->firstRow},{$page->listRows}")->select();
+        $goods_id_arr = get_arr_column($list, 'goods_id');
+        $goodsList = array();
+        if(!empty($goods_id_arr)){
+            $goodsList = M('goods')->where("goods_id in (".  implode(',',$goods_id_arr).")")->getField('goods_id,goods_name');
+        }
+        $this->assign('goodsList', $goodsList);
+        $this->assign('list', $list);
+        $this->assign('page', $page->show());// 赋值分页输出
+        $this->display();
+    }
+
+
 }
--- Application/Index/Controller/UserController.class.php
+++ Application/Index/Controller/UserController.class.php
@@ -93,21 +93,6 @@ class UserController extends BaseIndexController {
         $this->display();
     }
 
-    public function returnGoodsList(){
-        $count = M('return_goods')->where("user_id = {$this->user_id}")->count();
-        $page = new Page($count,10);
-        $list = M('return_goods')->where("user_id = {$this->user_id}")->order("id desc")->limit("{$page->firstRow},{$page->listRows}")->select();
-        $goods_id_arr = get_arr_column($list, 'goods_id');
-        $goodsList = array();
-        if(!empty($goods_id_arr)){
-            $goodsList = M('goods')->where("goods_id in (".  implode(',',$goods_id_arr).")")->getField('goods_id,goods_name');
-        }
-        $this->assign('goodsList', $goodsList);
-        $this->assign('list', $list);
-        $this->assign('page', $page->show());// 赋值分页输出
-        $this->display();
-    }
-
     public function coupon(){
         $usersLogic = new \Common\Logic\UsersLogic();
         $result = $usersLogic -> getCoupon( $this->user_id);
