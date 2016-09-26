b1610c395c35e81175e4ccc2facbf7275fa7de3c
钟瀚涛 <651799656@qq.com>
2016-09-26 18:06:56 +0800

    [修改]新样式

--- Application/Common/Common/Function/coupon.php
+++ Application/Common/Common/Function/coupon.php
@@ -1,19 +1,2 @@
 <?php
 
-//改变优惠券状态
-if( $this -> _post_data['useCoupon'] == true && !empty($this -> _post_data['couponInfo'])){
-    $condition = array(
-        "id" => $this -> _post_data["userCouponId"],
-        "uid" => $this -> user['user_id'],
-        "order_id" => 0,
-    );
-    $save = array(
-        "order_id" => $order["order_id"],
-        "use_time" => $this -> nowTime,
-    );
-
-    $result = M('coupon_list') -> where($condition) -> save($save);
-    if(empty($result)){
-        throw new \Exception('优惠券使用失败！');
-    }
-}
