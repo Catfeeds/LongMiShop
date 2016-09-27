79abbaa7b56d61681314438517473b906c986b4a
钟瀚涛 <651799656@qq.com>
2016-09-27 20:38:46 +0800

    [修改]忽略文件

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -1,17 +1,17 @@
 <?php
 
 
-//require_once "Function/base.php";
-//require_once "Function/order.php";
-//require_once "Function/user.php";
-//require_once "Function/verify.php";
-//require_once "Function/goods.php";
-//require_once "Function/email.php";
-//require_once "Function/service.php";
-//require_once "Function/address.php";
-//require_once "Function/log.php";
-//require_once "Function/weChat.php";
-//require_once "Function/callback.php";
+@require_once "Function/base.php";
+@require_once "Function/order.php";
+@require_once "Function/user.php";
+@require_once "Function/verify.php";
+@require_once "Function/goods.php";
+@require_once "Function/email.php";
+@require_once "Function/service.php";
+@require_once "Function/address.php";
+@require_once "Function/log.php";
+@require_once "Function/weChat.php";
+@require_once "Function/callback.php";
 
 function dd($x) {
     echo "<meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\">";
