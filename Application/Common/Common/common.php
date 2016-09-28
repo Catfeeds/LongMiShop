2d34a33dfa7983ba7947c2f3f7158b1c0048101c
钟瀚涛 <651799656@qq.com>
2016-09-28 18:39:39 +0800

    [修改]支付，库存部分

--- Application/Common/Common/common.php
+++ Application/Common/Common/common.php
@@ -626,33 +626,6 @@ function get_user_default_address($user_id){
 }
 
 
-/**
- * 支付完成修改订单
- * $order_sn 订单号
- * $pay_status 默认1 为已支付
- */
-function update_pay_status($order_sn,$pay_status = 1)
-{
-	// 如果这笔订单已经处理过了
-	$count = M('order')->where("order_sn = '$order_sn' and pay_status = 0")->count();   // 看看有没已经处理过这笔订单  支付宝返回不重复处理操作
-	if($count == 0) return false;
-	// 找出对应的订单
-	$order = M('order')->where("order_sn = '$order_sn'")->find();
-	// 修改支付状态  已支付
-	M('order')->where("order_sn = '$order_sn'")->save(array('pay_status'=>1,'pay_time'=>time()));
-	// 减少对应商品的库存
-	minus_stock($order['order_id']);
-	// 给他升级, 根据order表查看消费记录 给他会员等级升级 修改他的折扣 和 总金额
-	update_user_level($order['user_id']);
-	// 记录订单操作日志
-	logOrder($order['order_id'],'订单付款成功','付款成功',$order['user_id']);
-	//分销设置
-	M('rebate_log')->where("order_id = {$order['order_id']}")->save(array('status'=>1));
-	// 成为分销商条件
-	$distribut_condition = tpCache('distribut.condition');
-	if($distribut_condition == 1)  // 购买商品付款才可以成为分销商
-		M('users')->where("user_id = {$order['user_id']}")->save(array('is_distribut'=>1));
-}
 
     /**
      * 订单确认收货
