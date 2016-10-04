9666da13e4bcfe4a0997f587d1e3ea13f77dd371
钟瀚涛 <651799656@qq.com>
2016-10-04 18:26:39 +0800

    [调试]微信访问日志

--- Application/WeChat/Controller/WeChatController.class.php
+++ Application/WeChat/Controller/WeChatController.class.php
@@ -53,7 +53,7 @@ class WeChatController extends Controller {
         $keyword = trim($postObj->Content);
         $time = time();
 
-//        setLogResult(json_encode($postObj),"微信进来","test");
+        setLogResult(json_encode($postObj),"微信进来","test");
         //点击菜单拉取消息时的事件推送
         /*
          * 1、click：点击推事件
@@ -69,7 +69,7 @@ class WeChatController extends Controller {
         {
             $keyword = $this -> shopConfig['basic_subscribe_reply'];
         }
-
+        setLogResult($keyword,"微信进来keyword","test");
 
 
         if(empty($keyword)){
