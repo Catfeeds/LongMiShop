f91085142a47e030d3ca9deca4ab4f41909c3cbf
钟瀚涛 <651799656@qq.com>
2016-10-07 17:14:46 +0800

    [修改]轮播图排序

--- Application/Mobile/Controller/GoodsController.class.php
+++ Application/Mobile/Controller/GoodsController.class.php
@@ -172,7 +172,7 @@ class GoodsController extends MobileBaseController {
             $brnad = M('brand')->where("id =".$goods['brand_id'])->find();
             $goods['brand_name'] = $brnad['name'];
         }
-        $goods_images_list = M('GoodsImages')->where("goods_id = $goods_id")->select(); // 商品 图册
+        $goods_images_list = M('GoodsImages')->where("goods_id = $goods_id") -> order('sort')->select(); // 商品 图册
         $goods_attribute = M('GoodsAttribute')->getField('attr_id,attr_name'); // 查询属性
         $goods_attr_list = M('GoodsAttr')->where("goods_id = $goods_id")->select(); // 查询商品属性表
 		$filter_spec = $goodsLogic->getSpec($goods_id);
