b616dea6d88ffe4b81dd80a8b1dd310d59d1f50d
wenjirun <wenjirun@163.com>
2016-10-11 15:32:30 +0800

    [修改]微信提现测试

--- Application/Common/Common/Function/weChat.php
+++ Application/Common/Common/Function/weChat.php
@@ -276,3 +276,147 @@ function sendWeChatMessageUseUserId( $userId , $type , $data ){
     }
     return sendWeChatMessageUseUserInfo( $userInfo , $type , $data );
 }
+
+/*
+ * 企业向个人付款
+ * @param  access_token
+ * @param  openids 
+ * 
+ */
+
+function userWechatWithdrawDeposit($openids,$amounts,$nickname){
+	if( empty($openids) ){
+		return 'openid不能为空';
+	}
+	if($amounts < 100){
+		return '提现金额不能少于100';
+	}
+	$weChatConfig = M('wx_user')->find();
+    if( empty( $weChatConfig ) ){
+        return false;
+    }
+
+	$appid = $weChatConfig['openid']; 
+	$mch_appid = $appid;
+	$openid = $openids; //用户唯一标识
+	$mchid = '1394154902'; //商户号
+	$nonce_str = 'qyzf'.rand(100000, 999999); //随机数
+	$partner_trade_no = 'HW'.time().rand(10000, 99999); //商户订单号
+	$check_name = 'NO_CHECK';//校验用户姓名选项，NO_CHECK：不校验真实姓名 FORCE_CHECK：强校验真实姓名（未实名认证的用户会校验失败，无法转账）OPTION_CHECK：针对已实名认证的用户才校验真实姓名（未实名认证用户不校验，可以转账成功）
+	$re_user_name = $nickname;//收款用户姓名
+	$amount = $amounts;//金额（以分为单位，必须大于100）
+	$desc = '提现测试';//描述
+	$spbill_create_ip = $_SERVER["REMOTE_ADDR"];//请求ip
+
+	//封装成数据 
+	$dataArr=array();
+	$dataArr['amount']=$amount;
+	$dataArr['check_name']=$check_name;
+	$dataArr['desc']=$desc;
+	$dataArr['mch_appid']=$mch_appid;
+	$dataArr['mchid']=$mchid;
+	$dataArr['nonce_str']=$nonce_str;
+	$dataArr['openid']=$openid;
+	$dataArr['partner_trade_no']=$partner_trade_no;
+	$dataArr['re_user_name']=$re_user_name;
+	$dataArr['spbill_create_ip']=$spbill_create_ip;
+
+	$sign = getSign($dataArr);
+
+	$data="<xml>
+	<mch_appid>".$mch_appid."</mch_appid>
+	<mchid>".$mchid."</mchid>
+	<nonce_str>".$nonce_str."</nonce_str>
+	<partner_trade_no>".$partner_trade_no."</partner_trade_no>
+	<openid>".$openid."</openid>
+	<check_name>".$check_name."</check_name>
+	<re_user_name>".$re_user_name."</re_user_name>
+	<amount>".$amount."</amount>
+	<desc>".$desc."</desc>
+	<spbill_create_ip>".$spbill_create_ip."</spbill_create_ip>
+	<sign>".$sign."</sign>
+	</xml>";
+	return $data;
+
+	$ch = curl_init ();
+	$MENU_URL="https://api.mch.weixin.qq.com/mmpaymkttransfers/promotion/transfers";
+	curl_setopt ( $ch, CURLOPT_URL, $MENU_URL );
+	curl_setopt ( $ch, CURLOPT_CUSTOMREQUEST, "POST" );
+	curl_setopt ( $ch, CURLOPT_SSL_VERIFYPEER, FALSE );
+	curl_setopt ( $ch, CURLOPT_SSL_VERIFYHOST, FALSE );
+
+	$zs1="./apiclient_cert.pem";
+	$zs2="./apiclient_key.pem";
+	curl_setopt($ch,CURLOPT_SSLCERT,$zs1);
+	curl_setopt($ch,CURLOPT_SSLKEY,$zs2);
+	// curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; MSIE 5.01;
+	// Windows NT 5.0)');
+	curl_setopt ( $ch, CURLOPT_FOLLOWLOCATION, 1 );
+	curl_setopt ( $ch, CURLOPT_AUTOREFERER, 1 );
+	curl_setopt ( $ch, CURLOPT_POSTFIELDS, $data );
+	curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
+	$info = curl_exec ( $ch );
+
+	if (curl_errno ( $ch )) {
+		return curl_error ( $ch );
+	}
+
+	curl_close ( $ch );
+
+	return $info;
+
+
+
+}
+
+/**
+ * 	作用：格式化参数，签名过程需要使用
+ */
+function formatBizQueryParaMap($paraMap, $urlencode)
+{
+	// var_dump($paraMap);//die;
+	$buff = "";
+	ksort($paraMap);
+	foreach ($paraMap as $k => $v)
+	{
+		if($urlencode)
+		{
+			$v = urlencode($v);
+		}
+		//$buff .= strtolower($k) . "=" . $v . "&";
+		$buff .= $k . "=" . $v . "&";
+	}
+	$reqPar;
+	if (strlen($buff) > 0)
+	{
+		$reqPar = substr($buff, 0, strlen($buff)-1);
+	}
+	// var_dump($reqPar);//die;
+	return $reqPar;
+}
+
+/**
+ * 	作用：生成签名
+ */
+function getSign($Obj)
+{
+	// print_r($Obj);die;
+	foreach ($Obj as $k => $v)
+	{
+		$Parameters[$k] = $v;
+	}
+	//签名步骤一：按字典序排序参数
+	ksort($Parameters);
+	$String = formatBizQueryParaMap($Parameters, false);
+	//echo '【string1】'.$String.'</br>';
+	//签名步骤二：在string后加入KEY
+	$String = $String."&key=07OQtOhgnUDxWiQsx4shY90GI4sVWYrMWGHcMpmoJMN";
+	//echo "【string2】".$String."</br>";
+	//签名步骤三：MD5加密
+	$String = md5($String);
+	//echo "【string3】 ".$String."</br>";
+	//签名步骤四：所有字符转为大写
+	$result_ = strtoupper($String);
+	//echo "【result】 ".$result_."</br>";
+	return $result_;
+}
