a6e61ad3c2760113aa707b1a6e2d82170d88c11c
wenjirun <wenjirun@163.com>
2016-10-11 20:10:49 +0800

    [修改]提现测试23

--- Application/Common/Common/Function/weChat.php
+++ Application/Common/Common/Function/weChat.php
@@ -349,7 +349,7 @@ function userWechatWithdrawDeposit($openids,$amounts,$nickname){
 	curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,FALSE);
 	//设置header
     curl_setopt($ch,CURLOPT_HEADER,1);
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
 
 	// $zs1="http://" . $_SERVER['HTTP_HOST'] . "/Application/Common/Common/Function/apiclient_cert.pem";
 	// $zs2="http://" . $_SERVER['HTTP_HOST'] . "/Application/Common/Common/Function/apiclient_key.pem";
