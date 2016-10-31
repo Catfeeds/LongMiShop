708c3c59fc95bdc98ae9c7b96108a9673432bab6
钟瀚涛 <651799656@qq.com>
2016-10-31 17:35:40 +0800

    [修改]新的商品列表

--- Application/Admin/Controller/GoodsController.class.php
+++ Application/Admin/Controller/GoodsController.class.php
@@ -168,11 +168,11 @@ class GoodsController extends BaseController {
     public function ajaxGoodsList(){
 
         $where = ' 1 = 1 '; // 搜索条件
-        I('intro')    && $where = "$where and ".I('intro')." = 1" ;
-        I('brand_id') && $where = "$where and brand_id = ".I('brand_id') ;
-        (I('is_on_sale') !== '') && $where = "$where and is_on_sale = ".I('is_on_sale') ;
-        $cat_id = I('cat_id');
-        $cat_id = I('cat_id');
+//        I('intro')    && $where = "$where and ".I('intro')." = 1" ;
+//        I('brand_id') && $where = "$where and brand_id = ".I('brand_id') ;
+//        (I('is_on_sale') !== '') && $where = "$where and is_on_sale = ".I('is_on_sale') ;
+//        $cat_id = I('cat_id');
+//        $cat_id = I('cat_id');
         // 关键词搜索
         $key_word = I('key_word') ? trim(I('key_word')) : '';
         if($key_word)
@@ -180,16 +180,14 @@ class GoodsController extends BaseController {
             $where = "$where and (goods_name like '%$key_word%' or goods_sn like '%$key_word%')" ;
         }
 
-        if($cat_id > 0)
-        {
-            $grandson_ids = getCatGrandson($cat_id);
-            $where .= " and cat_id in(".  implode(',', $grandson_ids).") "; // 初始化搜索条件
-        }
+//        if($cat_id > 0)
+//        {
+//            $grandson_ids = getCatGrandson($cat_id);
+//            $where .= " and cat_id in(".  implode(',', $grandson_ids).") "; // 初始化搜索条件
+//        }
         if(is_supplier()){
             $where .= " and admin_id ='".session('admin_id')."'";
         }
-
-
 
         $model = M('Goods');
         $count = $model->where($where)->count();
@@ -200,9 +198,10 @@ class GoodsController extends BaseController {
         }
          */
         $show = $Page->show();
-        $order_str = "`{$_POST['orderby1']}` {$_POST['orderby2']}";
-        $goodsList = $model->where($where)->order($order_str)->limit($Page->firstRow.','.$Page->listRows)->select();
-
+        $orderby1 = I("orderby1","goods_id");
+        $orderby2 = I("orderby2","desc");
+        $order_str = "`{$orderby1}` {$orderby2}";
+        $goodsList = $model->where($where)->order($order_str)->limit($Page->firstRow.','.$Page->listRows) ->select();
         $catList = D('goods_category')->select();
         $catList = convert_arr_key($catList, 'id');
         $this->assign('catList',$catList);
