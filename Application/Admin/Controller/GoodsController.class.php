dcee9f9e5d2a4dc26dce4cde6338a48033d4ae8e
钟瀚涛 <651799656@qq.com>
2016-10-31 17:39:51 +0800

    [修改]供应商商品列表

--- Application/Admin/Controller/GoodsController.class.php
+++ Application/Admin/Controller/GoodsController.class.php
@@ -168,11 +168,11 @@ class GoodsController extends BaseController {
     public function ajaxGoodsList(){
 
         $where = ' 1 = 1 '; // 搜索条件
-//        I('intro')    && $where = "$where and ".I('intro')." = 1" ;
-//        I('brand_id') && $where = "$where and brand_id = ".I('brand_id') ;
-//        (I('is_on_sale') !== '') && $where = "$where and is_on_sale = ".I('is_on_sale') ;
-//        $cat_id = I('cat_id');
-//        $cat_id = I('cat_id');
+        I('intro')    && $where = "$where and ".I('intro')." = 1" ;
+        I('brand_id') && $where = "$where and brand_id = ".I('brand_id') ;
+        (I('is_on_sale') !== '') && $where = "$where and is_on_sale = ".I('is_on_sale') ;
+        $cat_id = I('cat_id');
+        $cat_id = I('cat_id');
         // 关键词搜索
         $key_word = I('key_word') ? trim(I('key_word')) : '';
         if($key_word)
@@ -180,11 +180,11 @@ class GoodsController extends BaseController {
             $where = "$where and (goods_name like '%$key_word%' or goods_sn like '%$key_word%')" ;
         }
 
-//        if($cat_id > 0)
-//        {
-//            $grandson_ids = getCatGrandson($cat_id);
-//            $where .= " and cat_id in(".  implode(',', $grandson_ids).") "; // 初始化搜索条件
-//        }
+        if($cat_id > 0)
+        {
+            $grandson_ids = getCatGrandson($cat_id);
+            $where .= " and cat_id in(".  implode(',', $grandson_ids).") "; // 初始化搜索条件
+        }
         if(is_supplier()){
             $where .= " and admin_id ='".session('admin_id')."'";
         }
