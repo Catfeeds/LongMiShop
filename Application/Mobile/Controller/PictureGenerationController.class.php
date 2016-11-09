47d5efe956ebe64f817ae569b4ffbcbed5573249
wenjirun <wenjirun@163.com>
2016-11-09 15:51:39 +0800

    [修改]装逼图 字体修改 空格替换

--- Application/Mobile/Controller/PictureGenerationController.class.php
+++ Application/Mobile/Controller/PictureGenerationController.class.php
@@ -26,13 +26,15 @@ class PictureGenerationController extends MobileBaseController {
 
     public function run(){
         $name = I("name","米兔");
+        $name = str_replace('+',' ',$name);
         $position = I("position","主编");
+        $position = str_replace('+',' ',$position);
         $im = imagecreatetruecolor(588, 800);
         $bg = imagecreatefromjpeg('./Template/mobile/longmi/Static/images/toutu.jpg');
         imagecopy($im,$bg,0,0,0,0,588, 800);
         imagedestroy($bg);
-        $black = imagecolorallocate($im, 0, 0, 0);
-        $font = './Template/mobile/longmi/Static/fonts/gb.ttf';
+        $black = imagecolorallocate($im, 115, 115, 115);
+        $font = './Template/mobile/longmi/Static/fonts/test.ttf';
         imagettftext($im, 24, 4, 100, 360, $black, $font, $name);
         imagettftext($im, 24, 4, 105, 400, $black, $font, $position);
         imagejpeg($im);
