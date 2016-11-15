fbb9000599e4868ab319e755e9fe04c206ad3ebc
钟瀚涛 <651799656@qq.com>
2016-11-15 17:36:29 +0800

    [修改]拉取openid没有的时候注册

--- Application/WeChat/Controller/ToolController.class.php
+++ Application/WeChat/Controller/ToolController.class.php
@@ -10,9 +10,14 @@ class ToolController extends Controller {
     public function index(){
         $weChatUserList = getWeChatUserList();
         if ( !empty( $weChatUserList ) ){
-            foreach ( $weChatUserList as $openid) {
+            foreach ( $weChatUserList as $openid ) {
+                echo $openid . ":";
+                if( ! isExistenceDataWithCondition( "users" , array( "openid" => $openid ) ) ){
+                    registerFromOpenid( $openid , array() , "WeChat" , false );
+                    echo "[注册]";
+                }
                 $res = weChatPullingMessage( $openid , false );
-                echo $openid . "[" . $res . "]<br>";
+                echo "[拉取：" . $res . "]<br>";
             }
         }
         echo "===========<br>";
