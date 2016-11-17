deaeb3136392659e8fcd349d88413e945240348e
钟瀚涛 <651799656@qq.com>
2016-11-17 15:45:05 +0800

    [修改]ajax 加载

--- Application/Mobile/Controller/GoodsController.class.php
+++ Application/Mobile/Controller/GoodsController.class.php
@@ -95,7 +95,7 @@ class GoodsController extends MobileBaseController {
     	$filter_attr  = $goodsLogic->get_filter_attr($filter_goods_id,$filter_param,'goodsList',1); // 获取指定分类下的帅选属性
 
     	$count = count($filter_goods_id);
-        $limit = 12;
+        $limit = 2;
     	$page = new Page($count,$limit);
     	if($count > 0)
     	{
