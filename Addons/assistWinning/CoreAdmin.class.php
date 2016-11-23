68d5a679bca402caecc9e766205c03e233375fe0
wenjirun <wenjirun@163.com>
2016-11-23 10:40:01 +0800

    [修改]烤面包 奖品设置

--- Addons/assistWinning/CoreAdmin.class.php
+++ Addons/assistWinning/CoreAdmin.class.php
@@ -12,7 +12,37 @@ class assistWinningAdminController {
 
     //初始页面
     public function index(){
+        $this -> assignData["list"] = array(
+            array(
+                "title" => "奖品设置",
+                "act"   => "setPrize"
+            ),
+            array(
+                "title" => "中奖列表",
+                "act"   => "prize"
+            )
+        );
+        return $this -> assignData;
 
+    }
+    public function setPrize(){
+        $list = M('addons_assistwinning_setprize')->find();
+        $this -> assignData['list'] = $list;
+        return $this -> assignData;
+    }
+
+    public function PostSet(){
+        $data = I('post.');
+        unset($data['pluginName']);
+        $data['uptatetime'] = time();
+        $res = M('addons_assistwinning_setprize')->save($data);
+        if($res){
+            exit(json_encode(callback(true,'修改成功')));
+        }
+        exit(json_encode(callback(false,'修改失败')));
+
+    }
+    public function prize(){
         $prefix = C('DB_PREFIX');
         $join = $prefix."users ON ".$prefix."addons_assistwinning_prize.user_id = ".$prefix."users.user_id";
         I('mobile') ? $condition[$prefix.'users.mobile'] = I('mobile') : false;
@@ -24,7 +54,8 @@ class assistWinningAdminController {
         foreach($condition as $key=>$val) {
             $Page->parameter[$key]   =   urlencode($val);
         }
-        $userList = M('addons_assistwinning_prize')->join($join)->where($condition)->limit($Page->firstRow.','.$Page->listRows)->select();
+        $field = $prefix.'addons_assistwinning_prize.*,'.$prefix.'users.nickname,'.$prefix.'users.user_id';
+        $userList = M('addons_assistwinning_prize')->field($field)->join($join)->where($condition)->limit($Page->firstRow.','.$Page->listRows)->select();
 
         $this -> assignData['page'] = $Page -> show();
         $this -> assignData['userList'] = $userList;
