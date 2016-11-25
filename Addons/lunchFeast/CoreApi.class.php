88631849f98e08f52983bc3f51ac199a1a2b2cf6
钟瀚涛 <651799656@qq.com>
2016-11-25 23:23:11 +0800

    [修改]接口调试

--- Addons/lunchFeast/CoreApi.class.php
+++ Addons/lunchFeast/CoreApi.class.php
@@ -29,17 +29,29 @@ class lunchFeastApiController
             "username" => $username,
             "password" => $password
         );
-        $userInfo = findDataWithCondition("addons_lunchfeast_admin",$condition , "is_lock");
+        $userInfo = findDataWithCondition("addons_lunchfeast_admin",$condition , "id,is_lock");
         if( empty( $userInfo ) ){
             exit(json_encode(callback(false,"没有此用户")));
         }
         if( !empty( $userInfo['is_lock'] ) && $userInfo['is_lock'] == 1 ){
             exit(json_encode(callback(false,"账号已被锁定，请联系管理员")));
         }
-        exit(json_encode(callback(true)));
+        $token = md5( $userInfo["id"]."_".time());
+        $condition2 = array(
+            "id" => $userInfo["id"]
+        );
+        $save = array(
+            "token" => $token,
+            "last_time" => time()
+        );
+        saveData( "addons_lunchfeast_admin" , $condition2 , $save );
+        exit(json_encode(callback(true,"",$token)));
     }
     //验证核销码
     public function verification(){
+        if( !lunchFeastApiUserToken(I("token")) ){
+            exit(json_encode(callback(false, "签名错误")));
+        }
         $code = I("code");
         $condition = array(
             "code" => $code,
@@ -51,10 +63,12 @@ class lunchFeastApiController
             exit(json_encode(callback(true)));
         }
         exit(json_encode(callback(false, "未找到相应的核销码")));
-
     }
-    //验证核销码
+    //使用核销码
     public function useCode(){
+        if( !lunchFeastApiUserToken(I("token")) ){
+            exit(json_encode(callback(false, "签名错误")));
+        }
         exit(json_encode(callback(true)));
     }
 }
