5670a9dc8c9583abdfbee81ffaf0d9f381b4654b
钟瀚涛 <651799656@qq.com>
2016-11-28 18:12:40 +0800

    [修改]邀请部分

--- Addons/lunchFeast/Function/base.php
+++ Addons/lunchFeast/Function/base.php
@@ -1,6 +1,146 @@
 <?php
 
 /**
+ * 生成推荐关系
+ * @param $userID
+ * @param $inviteUserId
+ * @param $nickname
+ * @param null $shopConfig
+ * @return bool
+ *
+ */
+function lunchFeastCreateInviteRelationship( $userID , $inviteUserId , $nickname , $shopConfig = null ){
+    if(
+        !empty( $userID ) &&
+        !empty( $inviteUserId ) &&
+        $userID != $inviteUserId &&
+        isExistenceDataWithCondition("users",array("user_id"=>$inviteUserId)) &&
+        !isExistenceDataWithCondition("addons_lunchfeast_invite_list",array( "user_id" =>$userID)) &&
+        !isExistenceDataWithCondition('addons_lunchfeast_order',array("user_id" => $userID,"status" => array("neq" => "0")))
+    ){
+        if( is_null( $shopConfig ) ){
+            $shopConfig = getLunchFeastConfig();
+        }
+        $addData = array(
+            "user_id"           => $userID,
+            "parent_user_id"    => $inviteUserId,
+            "create_time"       => time(),
+            "update_time"       => time(),
+        );
+        if(isSuccessToAddData( "addons_lunchfeast_invite_list" , $addData )){
+            lunchFeastGiveBeInviteGift( $userID );
+        }
+        if(  $shopConfig['invited_to'] == 1 ){
+            sendWeChatMessageUseUserId( $userID , "送券" , array("couponId" => $shopConfig['invited_to_value']) );
+        }
+        if(  $shopConfig['invite'] == 2 ){
+            sendWeChatMessageUseUserId( $inviteUserId , "成功邀请" , array( "userName" => $nickname ,"money" => $shopConfig['invite_value'] ) );
+        }
+        return true;
+    }
+    return false;
+}
+
+
+/**
+ * 获取被邀请奖励
+ * @param $userId
+ * @return bool
+ */
+function lunchFeastGiveBeInviteGift( $userId ){
+    $shopConfig = getLunchFeastConfig();
+    lunchFeastGiveGift( $userId , $shopConfig['invited_to_value'] , $shopConfig['invited_to'] , 0);
+    return true;
+}
+
+
+/**
+ * 获取邀请奖励
+ * @param $userId
+ * @return bool
+ */
+function lunchFeastGiveInviteGift( $userId ){
+    $condition = array(
+        "user_id" => $userId,
+        "status" => array("neq" => "0")
+    );
+    $orderCount = getCountWithCondition( 'addons_lunchfeast_order' , $condition );
+    if( $orderCount == 1){
+        $shopConfig = getLunchFeastConfig();
+        $invitedUserId = getInvitedUserId( $userId );
+        lunchFeastGiveGift( $invitedUserId , $shopConfig['invite_value'] , $shopConfig['invite'] , 1);
+        $userInfo = findDataWithCondition( "users" , array( "user_id" => $userId ) ,"nickname" );
+        $shopConfig = getLunchFeastConfig( );
+        if(  $shopConfig['invite'] == 2 ){
+            sendWeChatMessageUseUserId( $invitedUserId , "邀请奖励" , array("userName" => $userInfo['nickname'],"money" => $shopConfig['invite_value']) );
+        }
+        return true;
+    }
+    return false;
+}
+
+/**
+ * 获取奖励
+ * @param null $userID
+ * @param null $value
+ * @param int $type  1 为卡券  2 为余额 3 为积分
+ * @param $isInvite
+ * @return bool
+ */
+function lunchFeastGiveGift( $userID = null , $value = null , $type = 1 , $isInvite = 0 ){
+    if( is_null( $userID ) ){
+        return false;
+    }
+    $log = $isInvite ? "邀请奖励" : "系统奖励";
+    if( !is_null( $value ) ){
+        if( $type == 3 ){
+            accountLog( $userID , 0 , $value , $log);
+            return true;
+        }
+        if( $type == 2 ){
+            accountLog( $userID , $value , 0 , $log);
+            return true;
+        }
+        $add['cid'] = $value;
+        $add['type'] = 3;
+        $add['uid'] = $userID;
+        $add['send_time'] = time();
+        do{
+            $code = get_rand_str(8,0,1);//获取随机8位字符串
+            $check_exist = findDataWithCondition('coupon_list',array('code'=>$code),"code");
+            if( empty( $check_exist ) ){
+                $check_exist = findDataWithCondition('coupon_code',array('code'=>$code),"code");
+            }
+        }while($check_exist);
+        $add['code'] = $code;
+        M('coupon_list')->add($add);
+        return true;
+    }
+    return false;
+}
+
+
+/**
+ * 获取礼品情况
+ * @param null $value
+ * @param int $type
+ * @return array
+ */
+function lunchFeastGetGiftInfo( $value = null , $type = 1  ){
+    if( !is_null( $value ) ){
+        if( $type == 3 ){
+            return callback( true , "" ,array( 'point' => $value ,"type" => $type) );
+        }
+        if( $type == 2 ){
+            return callback( true , "" ,array( 'balance' => $value ,"type" => $type) );
+        }
+        $couponInfo = getCouponInfo($value);
+        return callback( true , "" ,array( 'coupon' => $couponInfo ,"type" => $type) );
+    }
+    return callback( false );
+}
+
+/**
  * 支付返回
  * @param $orderSn
  * @param $data
@@ -57,6 +197,14 @@ function addonsPayData( $orderId ){
         }
     }
     return $payData;
+}
+
+/**
+ * 获取配置
+ * @return mixed
+ */
+function getLunchFeastConfig(){
+    return findDataWithCondition( "addons_lunchfeast_config" );
 }
 /**
  * 用餐人置空
