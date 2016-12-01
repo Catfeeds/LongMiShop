32151a6baac74b11562f2f1ff1c45f12cb6e51e1
钟瀚涛 <651799656@qq.com>
2016-12-01 15:24:43 +0800

    [修改]删除无用部分

--- Application/Task/Conf/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-    'TMPL_PARSE_STRING'=>array(
-        '__PUBLIC__'        => '/Public/index',
-        '__MAIN__'        => '/Public',
-    ),
-    'TMPL_ACTION_SUCCESS' => 'Public:dispatch_jump',
-    'TMPL_ACTION_ERROR' => 'Public:dispatch_jump',
-);
