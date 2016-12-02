a805f84eff9f7cece0864670423917cd824e6d25
钟瀚涛 <651799656@qq.com>
2016-12-02 09:54:16 +0800

    [修改]短信通知

--- Application/Task/CronTab/Push.cron.php
+++ Application/Task/CronTab/Push.cron.php
@@ -14,26 +14,31 @@ class PushCronClass
     {
 
         $current = time();
-        $this->thisTime = date("Y-m-d",$current);
-        $this->nineItem = date("H",$current);
+        $this->thisTime = date("Y-m-d", $current);
+        $this->nineItem = date("H", $current);
 
-        if($this->nineItem == '09'){
-            $orderInfo = M('addons_lunchfeast_order')->where(array('status'=>1))->select();
-            setLogResult(  $orderInfo );
-            foreach($orderInfo as $item){
-                $orderTime = date('Y-m-d',$item['date']);
-                if($orderTime == $this->thisTime){
-                    $user = findDataWithCondition( 'users',array( "user_id" => $item['user_id'] ), 'openid' );
-                    $shopInfo = findDataWithCondition("addons_lunchfeast_shop",array('id'=>$item['shop_id']),'shop_name');
+        if ($this->nineItem == '09') {
+            $orderInfo = M('addons_lunchfeast_order')->where(array('status' => 1))->select();
+            foreach ($orderInfo as $item) {
+                $orderTime = date('Y-m-d', $item['date']);
+                if ($orderTime == $this->thisTime) {
+                    $user = findDataWithCondition('users', array("user_id" => $item['user_id']), 'openid,mobile,mobile_validated');
+                    $shopInfo = findDataWithCondition("addons_lunchfeast_shop", array('id' => $item['shop_id']), 'shop_name');
                     //$mealInfo = findDataWithCondition("addons_lunchfeast_meal_list",array('id'=>$orderInfo['meal_id']),'name');
-                    $text = "今天中午12:30，宴午".$shopInfo['shop_name']."期待您的大驾！人数：".$item['number'];
+                    $text = "今天中午12:30，宴午" . $shopInfo['shop_name'] . "期待您的大驾！人数：" . $item['number'];
                     $jsSdkLogic = new \Common\Logic\JsSdkLogic();
-                    $jsSdkLogic -> push_msg( $user['openid'] , $text );
+                    $jsSdkLogic->push_msg($user['openid'], $text);
+                    if ($user["mobile_validated"] == 1 && $user["mobile"]) {
+                        $mobileData = array(
+                            "time"   => "中午12:30",
+                            "place"  => "宴午" . $shopInfo['shop_name'],
+                            "number" => $item['number']
+                        );
+                        sendMobileMessages($user["mobile"], $mobileData, "SMS_32650055");
+                    }
                 }
             }
         }
-
-
     }
 }
 
