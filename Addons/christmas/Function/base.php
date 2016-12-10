c6465e219d1af1999a67bf67dc6acdd2fb724a28
钟瀚涛 <651799656@qq.com>
2016-12-11 00:18:57 +0800

    [修改]功能修改

--- Addons/christmas/Function/base.php
+++ Addons/christmas/Function/base.php
@@ -39,6 +39,22 @@ function addonsPayData( $orderId )
                 header('Location: ' . U('Mobile/Addons/christmas', array('pluginName' => 'orderDetail')));
                 exit;
             }
+            $activityInfo = findDataWithCondition("addons_christmas_activity");
+            $start_time = $activityInfo["start_time"];
+            $end_time = $activityInfo["end_time"];
+            if (time() < $start_time) {
+                mobileJumpToast( U('Mobile/Addons/christmas', array('pluginName' => 'rule')) , null , "活动还未开始" );
+                exit;
+            }
+            if ($end_time > time()) {
+                mobileJumpToast( U('Mobile/Addons/christmas', array('pluginName' => 'rule')) , null , "活动已经结束" );
+                exit;
+            }
+            $number = getCountWithCondition("addons_christmas_order", array("activity_id" => $activityInfo['id'],"status"=>array("neq","0")));
+            if ($number >= $activityInfo['number']) {
+                mobileJumpToast( U('Mobile/Addons/christmas', array('pluginName' => 'rule')) , null , "礼包名额已满" );
+                exit;
+            }
             $order["order_amount"] = $order["money"];
             $payData['order'] = $order;
             $payData['goUrl'] = U('Mobile/Addons/christmas', array("pluginName" => "results", "order_id" => $id));
