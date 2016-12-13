92c59ab7ba049a64ef7a2e1a538bc06b07f2d7bb
钟瀚涛 <651799656@qq.com>
2016-12-13 17:31:03 +0800

    [修改]样式和loading效果

--- Addons/christmas/Static/js/index.js
+++ Addons/christmas/Static/js/index.js
@@ -98,12 +98,18 @@ $(function(){
 	var blHeight = screenWidth * 568/320;
 	$(".page_div img").each(function(){
 		var myTop = $(this).css("top");
+		var myLeft = $(this).css("left");
 		var myWidth = $(this).css("width");
 		var myHeight = $(this).height();
+		var myWidth2 = $(this).width();
 		var reCat = /^((\d+\.?\d*)|(\d*\.\d+))\%$/;
 		if(reCat.test(myTop)){
 			var newTop = screenHeight * toPoint(myTop);
 			$(this).css('top',newTop+ 'px');
+		}
+		if(reCat.test(myLeft)){
+			var newLeft = myWidth2 * toPoint(myLeft);
+			$(this).css('left',newLeft+ 'px');
 		}
 		if(reCat.test(myWidth)){
 			var newWidth = screenWidth * toPoint(myWidth);
@@ -131,3 +137,39 @@ function toPoint(percent){
 	str= str/100;
 	return str;
 }
+
+$(function(){
+	var t_img; // 定时器
+	var isLoad = true; // 控制变量
+	// var imgNum=$('img').length;
+// 判断图片加载状况，加载完成后回调
+	isImgLoad(function(){
+		// 加载完成
+		$("#loading").hide();
+	});
+
+// 判断图片加载的函数
+	function isImgLoad(callback){
+		// 注意我的图片类名都是cover，因为我只需要处理cover。其它图片可以不管。
+		// 查找所有封面图，迭代处理
+		$('img').each(function(){
+			// 找到为0就将isLoad设为false，并退出each
+			if(this.height === 0){
+				isLoad = false;
+				return false;
+			}
+		});
+		// 为true，没有发现为0的。加载完毕
+		if(isLoad){
+			clearTimeout(t_img); // 清除定时器
+			// 回调函数
+			callback();
+			// 为false，因为找到了没有加载完成的图，将调用定时器递归
+		}else{
+			isLoad = true;
+			t_img = setTimeout(function(){
+				isImgLoad(callback); // 递归扫描
+			},500); // 我这里设置的是500毫秒就扫描一次，可以自己调整
+		}
+	}
+});
