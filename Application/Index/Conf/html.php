5d740161e7356ed968c3d23660c737b18bf157cf
钟瀚涛 <651799656@qq.com>
2016-12-16 11:08:47 +0800

    [修改]新的架子

---
+++ Application/Index/Conf/html.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'VIEW_PATH'       =>'./Template/index/', // 改变某个模块的模板文件目录
+    'DEFAULT_THEME'    =>'lepur', // 模板名称
+
+    'TMPL_PARSE_STRING'=>array(
+        '__INDEX__'        => '/Template/index/lepur/Static',
+        '__PUBLIC__'        => '/Public',
+        '__STATIC__'         => '/Template/index/lepur/Static',
+    ),
+    //'DATA_CACHE_TIME'=>60, // 查询缓存时间
+);
