523d64206685cbf2429862b180ff62739b398387
钟瀚涛 <651799656@qq.com>
2016-12-16 15:28:42 +0800

    [修改]新插件  米粒游戏

--- Application/Index/Conf/html.php
+++ Application/Index/Conf/html.php
@@ -1,13 +1,13 @@
 <?php
 return array(
     'VIEW_PATH'       =>'./Template/index/', // 改变某个模块的模板文件目录
-//    'DEFAULT_THEME'    =>'lepur', // 模板名称
-    'DEFAULT_THEME'    =>'default', // 模板名称
+    'DEFAULT_THEME'    =>'lepur', // 模板名称 lepur
+//    'DEFAULT_THEME'    =>'default', // 模板名称 default
 
     'TMPL_PARSE_STRING'=>array(
-        '__INDEX__'        => '/Template/index/default/Static',
+        '__INDEX__'        => '/Template/index/lepur/Static',
         '__PUBLIC__'        => '/Public',
-        '__STATIC__'         => '/Template/index/default/Static',
+        '__STATIC__'         => '/Template/index/lepur/Static',
     ),
-    //'DATA_CACHE_TIME'=>60, // 查询缓存时间
+//    'DATA_CACHE_TIME'=>60, // 查询缓存时间
 );
