266e72715230ca208ece0430cdf8cdf8f86fc555
钟瀚涛 <651799656@qq.com>
2016-12-19 10:21:29 +0800

    [修改]官网复原

--- Application/Index/Conf/html.php
+++ Application/Index/Conf/html.php
@@ -1,13 +1,13 @@
 <?php
 return array(
     'VIEW_PATH'       =>'./Template/index/', // 改变某个模块的模板文件目录
-    'DEFAULT_THEME'    =>'lepur', // 模板名称 lepur
-//    'DEFAULT_THEME'    =>'default', // 模板名称 default
+//    'DEFAULT_THEME'    =>'lepur', // 模板名称 lepur
+    'DEFAULT_THEME'    =>'default', // 模板名称 default
 
     'TMPL_PARSE_STRING'=>array(
-        '__INDEX__'        => '/Template/index/lepur/Static',
+        '__INDEX__'        => '/Template/index/default/Static',
         '__PUBLIC__'        => '/Public',
-        '__STATIC__'         => '/Template/index/lepur/Static',
+        '__STATIC__'         => '/Template/index/default/Static',
     ),
 //    'DATA_CACHE_TIME'=>60, // 查询缓存时间
 );
