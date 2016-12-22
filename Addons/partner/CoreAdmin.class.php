ee017404b6f362290569a72ef1e082f0345cdc72
钟瀚涛 <651799656@qq.com>
2016-12-22 11:51:47 +0800

    [修改]后台修改

--- Addons/partner/CoreAdmin.class.php
+++ Addons/partner/CoreAdmin.class.php
@@ -1,24 +1,36 @@
 <?php
+@include 'Addons/partner/Function/base.php';
 
 class partnerAdminController {
+
+    const TB_LIST = "addons_partner_list";
 
     public $assignData = array();
 
     public function __construct()
     {
     }
+
+
     //初始页面
     public function index(){
         $this->assignData["list"] = array(
             array(
-                "title" => "基础设置",
-                "act"   => "activitySet"
-            ),
-            array(
-                "title" => "订单列表",
-                "act"   => "orderList"
+                "title" => "申请列表",
+                "act"   => "myList"
             )
         );
         return $this->assignData;
     }
+
+    public function myList(){
+        $count = getCountWithCondition(TB_LIST);
+        $Page  = new \Think\Page( $count , 10 );
+        $show = $Page -> show();
+        $this->assignData['list'] = M(TB_SHOP)->limit($Page->firstRow,$Page->listRows) -> select();
+        $this->assignData['page'] = $show;
+        return $this->assignData;
+        return $this->assignData;
+    }
+
 }
