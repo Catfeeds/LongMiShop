3b28bb7d3d1142560adebc92b1914e25688251fd
钟瀚涛 <651799656@qq.com>
2016-12-28 21:32:12 +0800

    [修改]版本号

--- Application/Common/Base/BaseController.class.php
+++ Application/Common/Base/BaseController.class.php
@@ -29,7 +29,7 @@ class BaseController extends Controller
         $shopConfig = getShopConfig();
         $this -> shopConfig = $shopConfig;
         $this -> assign('shopConfig', $shopConfig);
-        $versionToken = "v2.2".time();
+        $versionToken = "v2.3";
         $this -> assign('versionToken', $versionToken);
     }
 
