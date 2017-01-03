2c6c8c6cfdccf68dd90d2586f396b3ba4665df27
钟瀚涛 <651799656@qq.com>
2017-01-03 11:39:22 +0800

    [修改]关闭静态页面

--- Application/Mobile/Conf/html.php
+++ Application/Mobile/Conf/html.php
@@ -10,13 +10,13 @@ return array(
         '__DefaultGoodsgroundImages__'=>'/Public/images/default/goodsimg.jpg',
     ),
 
-    'HTML_CACHE_ON'     =>   true, // 开启静态缓存
+    'HTML_CACHE_ON'     =>   false, // 开启静态缓存
     'HTML_CACHE_TIME'   =>   60,   // 全局静态缓存有效期（秒）
     'HTML_FILE_SUFFIX'  =>   '.html', //设置静态缓存文件后缀
     'HTML_CACHE_RULES'  =>   array(  //定义静态缓存规则
         // 定义格式1 数组方式
         //'静态地址' =>  array('静态规则', '有效期', '附加规则'),
-        'Index:index'=>array('{:module}_{:controller}_{:action}',60),  // 首页静态缓存 3秒钟
+//        'Index:index'=>array('{:module}_{:controller}_{:action}',60),  // 首页静态缓存 3秒钟
         //'index:goodsList'=>array('{:module}_{:controller}_{:action}',300),  // 列表页静态缓存 3秒钟 无参数 post 提交的很难缓存
         //'index:goodsList'=>array('{:module}_{:controller}_{:action}_{id}',TPSHOP_CACHE_TIME),  // 列表页静态缓存 3秒钟
         //ajax 请求的商品列表内容在 ajaxGoodsList 函数中  S($keys,$html,300); 缓存
