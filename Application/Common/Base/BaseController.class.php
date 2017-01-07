fa6b9219d051f6b3b727457cdb955e34b71589fb
钟瀚涛 <651799656@qq.com>
2017-01-07 17:12:30 +0800

    [修改]会员中心的效果

--- Application/Common/Base/BaseController.class.php
+++ Application/Common/Base/BaseController.class.php
@@ -29,7 +29,7 @@ class BaseController extends Controller
         $shopConfig = getShopConfig();
         $this -> shopConfig = $shopConfig;
         $this -> assign('shopConfig', $shopConfig);
-        $versionToken = "v2.9";
+        $versionToken = "v3.0";
         $this -> assign('versionToken', $versionToken);
     }
 
