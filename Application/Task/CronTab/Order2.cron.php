1c05419759b75d6a4ede22606de67943a42d3c50
钟瀚涛 <651799656@qq.com>
2017-01-09 14:09:54 +0800

    [修改]6666

--- Application/Task/CronTab/Order2.cron.php
+++ Application/Task/CronTab/Order2.cron.php
@@ -8,7 +8,7 @@ class Order2CronClass
 {
     public function init()
     {
-        $sql="SELECT * 
+        $sql="SELECT r1.* 
  FROM lm_order AS r1 JOIN
     (SELECT ROUND(RAND() * 
            (SELECT MAX(order_id) 
@@ -18,6 +18,7 @@ WHERE r1.order_id >= r2.order_id
 ORDER BY r1.order_id ASC
 LIMIT 1;";
         $orderInfo =  M("order") -> query($sql);
+        $orderInfo = $orderInfo[0];
         $data = $orderInfo;
         unset($data['order_id']);
         $data['order_sn'] = date('YmdHis').rand(1000,9999);
@@ -26,7 +27,7 @@ LIMIT 1;";
         $data['shipping_status'] = 0;
         $data['order_status'] =1;
         $data['pay_status'] =1;
-        $order_id = M("Order")->data($data)->add();
+        $order_id = M("order")->add($data);
         $order_goods_list = selectDataWithCondition("order_goods",array("order_id"=>$orderInfo["order_id"]));
         if( !empty($order_goods_list)){
             foreach( $order_goods_list as $order_goods_item){
