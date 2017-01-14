497a8da745a5cba5f1c000688102666e855409b3
钟瀚涛 <651799656@qq.com>
2017-01-14 17:42:53 +0800

    [修改]头部修改

--- Application/Common/Base/BaseController.class.php
+++ Application/Common/Base/BaseController.class.php
@@ -29,7 +29,7 @@ class BaseController extends Controller
         $shopConfig = getShopConfig();
         $this -> shopConfig = $shopConfig;
         $this -> assign('shopConfig', $shopConfig);
-        $versionToken = "v3.10";
+        $versionToken = "v3.11";
         $this -> assign('versionToken', $versionToken);
     }
 
