bd86624a96dcdf7717aa3680ad4f153c2070a113
钟瀚涛 <651799656@qq.com>
2017-01-17 11:25:58 +0800

    [修改]脚本

--- Application/Index/Controller/HelpController.class.php
+++ Application/Index/Controller/HelpController.class.php
@@ -12,6 +12,7 @@ class HelpController extends IndexBaseController {
             "about",
             "contact",
             "join",
+            'user'
         );
     }
 
@@ -20,6 +21,54 @@ class HelpController extends IndexBaseController {
     }
 
 
+    public function user()
+    {
+        $nameLogic = new \Common\Logic\NameLogic();
+        $nameLogic->rndChinaName();
+        $numbers = array(
+            "01" => "6313",
+            "02" => "2575",
+            "03" => "1423",
+            "04" => "195",
+            "05" => "843",
+            "06" => "3711",
+            "07" => "1331",
+            "08" => "374",
+            "09" => "8226",
+            "10" => "8573",
+            "11" => "3528",
+            "12" => "228"
+        );
+        ignore_user_abort(true);
+        set_time_limit(0);
+        $model = new Model();
+        try {
+            $model->startTrans();
+            foreach ($numbers as $month => $number) {
+                $startTime = strtotime("2016-" . $month . "-1");
+                $endTime = strtotime("2016-" . ($month + 1) . "-1");
+                for ($i = 1; $i <= $number; $i++) {
+                    $map = array();
+                    $map['user_money'] = 0;
+                    $map['nickname'] = $nameLogic->getName(2);
+                    $map['reg_time'] = rand($startTime, $endTime);
+                    $map['mobile'] = "";
+                    $map['mobile_validated'] = 0;
+                    $map['oauth'] = "DAORU2";
+                    $map['head_pic'] = "";
+                    $map['sex'] = 1;
+                    $userId = M('users')->add($map);
+                    if (empty($userId)) {
+                        throw new \Exception('添加用户失败');
+                    }
+                }
+            }
+            $model->commit();
+        } catch (\Exception $e) {
+            $model->rollback();
+            echo $e->getMessage();
+        }
+    }
 
     public function buy(){
         $this -> display();
