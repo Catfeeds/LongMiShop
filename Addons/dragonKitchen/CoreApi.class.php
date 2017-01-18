d790326d8f0830f7a0545a001da407e2c8daa4c1
钟瀚涛 <651799656@qq.com>
2017-01-18 16:09:35 +0800

    [修改]一个测试

--- Addons/dragonKitchen/CoreApi.class.php
+++ Addons/dragonKitchen/CoreApi.class.php
@@ -1,15 +1,26 @@
 <?php
+
 @include 'Addons/dragonKitchen/Function/base.php';
+
 class dragonKitchenApiController
 {
 
     public function __construct()
     {
+        $signArray = array("sign_time" => I("sign_time"), "sign_str" => I("sign_str"));
+        if (!signVerification($signArray)) {
+            printJson(false, "验证失败!");
+        }
     }
 
 
+    public function index()
+    {
+    }
 
-    public function index(){
+
+    public function getList(){
+
     }
 
 
