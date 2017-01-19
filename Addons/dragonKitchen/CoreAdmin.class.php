fc717b4fb065aba5374a5eae0261300d47dcf4e5
钟瀚涛 <651799656@qq.com>
2017-01-19 17:57:30 +0800

    [修改]插件部分整改

--- Addons/dragonKitchen/CoreAdmin.class.php
+++ Addons/dragonKitchen/CoreAdmin.class.php
@@ -13,7 +13,30 @@ class dragonKitchenAdminController
 
     public function index()
     {
-
+        $this->assignData["list"] = array(
+            array(
+                "title" => "食谱设置",
+                "act"   => "recipes"
+            ),
+            array(
+                "title" => "分类设置",
+                "act"   => "classify"
+            )
+        );
+        return $this->assignData;
     }
 
+
+
+
+    public function recipes(){
+        return $this->assignData;
+    }
+    public function recipesDetail(){
+        return $this->assignData;
+    }
+
+    public function classify(){
+        return $this->assignData;
+    }
 }
