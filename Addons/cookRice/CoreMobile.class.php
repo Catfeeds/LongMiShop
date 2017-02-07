11e209d3f430ca8b8cb0eb2e8cca6208b4a5e7cd
钟瀚涛 <651799656@qq.com>
2017-02-07 10:42:29 +0800

    [修改]版本修改

--- Addons/cookRice/CoreMobile.class.php
+++ Addons/cookRice/CoreMobile.class.php
@@ -15,7 +15,7 @@ class cookRiceMobileController
 
     public function __construct($userInfo)
     {
-        $this->assignData["v"] = "v1.9";
+        $this->assignData["v"] = "v2.0";
         $this->user = $userInfo;
         $this->config = cookRiceGetConfig();
         $this->edition = $this->config["edition"];
