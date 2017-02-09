f8f6eb4a68ac36ed139a3492e0c93d974f229c95
钟瀚涛 <651799656@qq.com>
2017-02-09 17:18:03 +0800

    [修改]农行测试

---
+++ Addons/agriculturalBank/CoreAdmin.class.php
@@ -0,0 +1,27 @@
+<?php
+@include 'Addons/agriculturalBank/Function/base.php';
+
+class agriculturalBankAdminController
+{
+
+    const TB_LIST = "addons_agriculturalbank_list";
+
+    public $assignData = array();
+
+    public function __construct()
+    {
+    }
+
+
+    //初始页面
+    public function index()
+    {
+        $count = getCountWithCondition(self::TB_LIST);
+        $Page = new \Think\Page($count, 10);
+        $show = $Page->show();
+        $this->assignData['list'] = M(self::TB_LIST)->limit($Page->firstRow, $Page->listRows)->order(" create_time desc")->select();
+        $this->assignData['page'] = $show;
+        return $this->assignData;
+    }
+
+}
