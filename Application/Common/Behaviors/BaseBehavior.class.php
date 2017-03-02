82f1391f2d34c1bcddeeb9d7029604af2100186f
钟瀚涛 <651799656@qq.com>
2017-03-02 11:45:36 +0800

    [修改]去掉重复部分

--- Application/Common/Behaviors/BaseBehavior.class.php
+++ Application/Common/Behaviors/BaseBehavior.class.php
@@ -9,21 +9,24 @@ class BaseBehavior extends Behavior{
         }
 
         if(MODULE_NAME == "Admin"){
-            if( isMobile() ){
-                C("DEFAULT_THEME","Moving");
-                C("TMPL_PARSE_STRING.__STATIC__","/Template/admin/Moving/Static");
-                $routeArray =  array(
-                    '/Admin/Index/index'=>'/Admin/Index/welcome',
-                );
+//            if( isMobile() ){
+//                C("DEFAULT_THEME","Moving");
+//                C("TMPL_PARSE_STRING.__STATIC__","/Template/admin/Moving/Static");
+//                $routeArray =  array(
+//                    '/Admin/Index/index'=>'/Admin/Index/welcome',
+//                );
+//
+//                if(!empty($routeArray[__ACTION__])){
+//                    header("Location: ".U($routeArray[__ACTION__])."");
+//                    exit;
+//                }
+//            }else{
+//                C("DEFAULT_THEME","new");
+//                C("TMPL_PARSE_STRING.__STATIC__","/Template/admin/new/Static");
+//            }
 
-                if(!empty($routeArray[__ACTION__])){
-                    header("Location: ".U($routeArray[__ACTION__])."");
-                    exit;
-                }
-            }else{
-                C("DEFAULT_THEME","new");
-                C("TMPL_PARSE_STRING.__STATIC__","/Template/admin/new/Static");
-            }
+            C("DEFAULT_THEME","new");
+            C("TMPL_PARSE_STRING.__STATIC__","/Template/admin/new/Static");
         }
     }
 }
