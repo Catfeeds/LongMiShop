7cdc1e4b79fce699744cf960825d3849e99f6fac
zhonght <651799656@qq.com>
2017-04-16 21:49:22 +0800

    [新增]早餐打卡模块

---
+++ Addons/breakFast/CoreAdmin.class.php
@@ -0,0 +1,80 @@
+<?php
+@include 'Addons/breakFast /Function/base.php';
+
+class breakFastAdminController
+{
+
+    const TB_CONFIG = "addons_breakfast_config";
+    const TB_DATA = "addons_breakfast_data";
+
+    public $assignData = array();
+
+    public function __construct()
+    {
+    }
+
+    public function index()
+    {
+
+        $this->assignData["list"] = array(
+            array(
+                "title" => "基础设置",
+                "act"   => "config"
+            ),
+            array(
+                "title" => "打卡列表",
+                "act"   => "lists"
+            ),
+//            array(
+//                "title" => "统计",
+//                "act"   => "statistics"
+//            ),
+
+        );
+        return $this->assignData;
+    }
+
+    public function statistics(){
+        return $this->assignData;
+    }
+    public function config()
+    {
+        if( IS_POST ){
+            $dataList = I("config");
+            if( !empty($dataList)){
+                foreach ($dataList as $dataKey => $dataItem){
+                    $condition = array("key_name" => $dataKey);
+                    if(getCountWithCondition(self::TB_CONFIG,$condition)>0){
+                        $data = array("val" =>$dataItem);
+                        saveData(self::TB_CONFIG,$condition,$data);
+                    }else{
+                        $data = array("key_name" => $dataKey,"val" =>$dataItem);
+                        addData(self::TB_CONFIG,$data);
+                    }
+                }
+                return addonsError("保存成功");
+            }
+            return addonsError("非法访问");
+        }
+        $this->assignData["config"] = break_fast_get_config();
+        return $this->assignData;
+    }
+
+    public function lists()
+    {
+        $count = getCountWithCondition(self::TB_DATA);
+        $Page = new \Think\Page($count, 10);
+        $show = $Page->show();
+        $lists =  M(self::TB_DATA)->limit($Page->firstRow, $Page->listRows)->order(" create_time desc")->select();
+        if(!empty($lists)){
+            foreach ($lists as $key =>$list){
+                $user = findDataWithCondition("users",array('user_id'=>$list['user_id']),"nickname");
+                $lists[$key]["nickname"] = $user["nickname"];
+            }
+        }
+        $this->assignData['list'] =$lists;
+        $this->assignData['page'] = $show;
+        return $this->assignData;
+    }
+
+}
---
+++ Addons/breakFast/CoreMobile.class.php
@@ -0,0 +1,82 @@
+<?php
+@include 'Addons/breakFast /Function/base.php';
+
+class breakFastMobileController
+{
+
+    const TB_CONFIG = "addons_breakfast_config";
+    const TB_DATA = "addons_breakfast_data";
+
+    public $userInfo = null;
+    public $assignData = array();
+
+
+    //初始化
+    public function __construct($userInfo)
+    {
+        $this->assignData["userInfo"] = $this->userInfo = $userInfo;
+
+        $this->assignData["headerPath"] = "./Addons/breakFast/Template/Mobile/default/Addons_header.html";
+        $this->assignData["sharePath"] = "./Addons/breakFast/Template/Mobile/default/Addons_wxShare.html";
+
+        $this->assignData["config"] = $config= break_fast_get_config();
+
+        $weChatLogic = new \Common\Logic\WeChatLogic();
+        $this->assignData["signPackage"] = $weChatLogic->getSignPackage();
+        $this->assignData["shareData"] = array(
+            "title" => empty($config['share_title'])?"龙米早餐打卡计划":$config['share_title'],
+            "desc" => empty($config['share_desc'])?"一起来打卡吧！":$config['share_desc'],
+            "img" => empty($config['share_img'])?"http://" . $_SERVER["HTTP_HOST"] . "/Template/index/default/Static/images/sh-02.png":$config['share_img'],
+            "url" => "http://" . $_SERVER["HTTP_HOST"] . U("Mobile/Addons/breakFast")
+        );
+    }
+
+    //首页
+    public function index()
+    {
+        return $this->assignData;
+    }
+
+
+    //提交结果
+    public function over()
+    {
+        $status = break_fast_get_status();
+        if ($status == 1) {
+            $date = break_fast_get_this_day();
+            $condition = array("date" => $date, "user_id" => $this->userInfo['user_id']);
+            if (getCountWithCondition(self::TB_DATA, $condition) > 0) {
+                $newStatus = "2";
+            } else {
+                if (break_fast_is_hour()) {
+                    $condition['create_time'] = time();
+                    addData(self::TB_DATA, $condition);
+                    if(accountLog($this->userInfo['user_id'],1,0,"早餐打卡")){
+                        $text = "打卡成功！1元余额已到达你的账户【<a href=\"".U('Mobile/User/account')."\">点击查看</a>】";
+                        $jsSdkLogic = new \Common\Logic\JsSdkLogic();
+                        $jsSdkLogic->push_msg($this->userInfo['openid'], $text);
+                    }
+                    $newStatus = "1";
+                } else {
+                    $newStatus = "-3";
+                }
+            }
+        } else if ($status == 2) {
+            $newStatus = "-1";
+        } else {
+            $newStatus = "-2";
+        }
+        $textArray = array(
+            "-3" => "噢，本日打卡时间已过，请明天8:00~10:00再来哦",
+            "-2" => "活动已经结束",
+            "-1" => "活动还没开始",
+            "1" => "打卡成功！1元余额已到达你的账户",
+            "2" => "本日已经打卡成功了，请明天再来",
+        );
+        $this->assignData["status"] = $newStatus;
+        $this->assignData["textArray"] = $textArray;
+
+        return $this->assignData;
+    }
+
+}
