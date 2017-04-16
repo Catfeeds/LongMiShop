2daab521d29eb425eb82ea59a4316aa671031143
zhonght <651799656@qq.com>
2017-04-16 22:09:23 +0800

    [调试]

--- Addons/breakFast/Function/base.php
+++ Addons/breakFast/Function/base.php
@@ -22,7 +22,7 @@ function break_fast_get_this_day()
 function break_fast_is_hour()
 {
     $myHour = date("H", time());
-    $hourArray = array("08", "09","20","21");
+    $hourArray = array("08", "09","20","21","22");
     if (in_array($myHour, $hourArray)) {
         return true;
     }
