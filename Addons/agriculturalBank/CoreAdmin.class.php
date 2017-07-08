b24313137bafe64a4a9eb03fa657ae968e83b712
zhonght <651799656@qq.com>
2017-07-08 22:56:21 +0800

    [修改]农行显示订单信息

--- Addons/agriculturalBank/CoreAdmin.class.php
+++ Addons/agriculturalBank/CoreAdmin.class.php
@@ -23,9 +23,29 @@ class agriculturalBankAdminController
         if( !empty($list)){
             foreach ($list as $key => $item){
                 $list[$key]["user"] = get_user_info($item["user_id"]);
+                $condition = "user_id ='". $item["user_id"]."' and add_time >= '".$item['create_time']."' and pay_status = 1";
+                $count = getCountWithCondition("order", $condition);
+                $lists[$key]["orderCount"] = intval($count);
             }
         }
         $this->assignData['list'] = $list;
+        $this->assignData['page'] = $show;
+        return $this->assignData;
+    }
+    //初始页面
+    public function order()
+    {
+        $id = I('id');
+        $user_id = I('user_id');
+        $info = findDataWithCondition(self::TB_LIST, array("id" => $id));
+        $condition = "user_id ='".$user_id."' and add_time >= '".$info['create_time']."' and pay_status = 1";
+        $count = getCountWithCondition("order", $condition);
+        $Page = new \Think\Page($count, 10);
+        $show = $Page->show();
+        $lists =M("order")->where($condition)->limit($Page->firstRow, $Page->listRows)->order(" add_time desc")->select();
+        $orderLogic = new Admin\Logic\OrderLogic();
+        $lists = $orderLogic -> getOrderListInfo( $lists );
+        $this->assignData['list'] =$lists;
         $this->assignData['page'] = $show;
         return $this->assignData;
     }
