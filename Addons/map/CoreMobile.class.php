7308f53de90a079730fb2985e678fa429388f756
zhonght <651799656@qq.com>
2017-08-30 23:28:41 +0800

    [修改]提示

--- Addons/map/CoreMobile.class.php
+++ Addons/map/CoreMobile.class.php
@@ -7,7 +7,7 @@ class mapMobileController {
     public function __construct()
     {
         $html = M("addons_map_html")->where(array('id'=>1))->getField("html");
-        echo $html;
+        echo htmlspecialchars_decode($html);
         exit;
     }
 
