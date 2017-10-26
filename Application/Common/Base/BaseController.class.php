43876c1032582b0d488349caed994df3b8d06e5f
zhonght <651799656@qq.com>
2017-10-26 20:37:09 +0800

    [修改]新的首页（孙）

--- Application/Common/Base/BaseController.class.php
+++ Application/Common/Base/BaseController.class.php
@@ -29,7 +29,7 @@ class BaseController extends Controller
         $shopConfig = getShopConfig();
         $this -> shopConfig = $shopConfig;
         $this -> assign('shopConfig', $shopConfig);
-        $versionToken = "v3.18";
+        $versionToken = "v3.19";
         $this -> assign('versionToken', $versionToken);
     }
 
